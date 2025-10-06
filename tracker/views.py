8334cebb67d3c357c87c5b1477024a1e90aee9ad
Joshua Ejiofor <jue3@scarletmail.rutgers.edu>
2025-10-06 22:57:47 +0000

    Views dashboard

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Games
 
 # Create your views here.
+def _dashboard_(request):
+    game=Games.objects.all().order_ny("-Date played")
+    return render(request, 'tracker/admin__dashboard.html',{'games':game})
+    
