97c6557e173286a8aab1128edfc43cf440f26559
Joshua Ejiofor <jue3@scarletmail.rutgers.edu>
2025-10-06 23:04:22 +0000

    Admin dashboard added

--- sportstracker_app/urls.py
+++ sportstracker_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("admin-dashboard/", views.admin_dashboard, name="dashboard"),
 ]
