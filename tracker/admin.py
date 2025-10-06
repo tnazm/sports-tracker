9039f1178b2ebb5b34a3e55e6231fc4fa951e442
Joshua Ejiofor <jue3@scarletmail.rutgers.edu>
2025-10-06 22:06:29 +0000

    Games model added

--- tracker/admin.py
+++ tracker/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Games
 
 # Register your models here.
+class Games_Admin(admin.ModelAdmin):
+    variables=('player_name','opponent_name', 'sport','date_game_played','score', 'result')
+    
