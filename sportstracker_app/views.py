fb480492867797d77a48b3d17155952cbbdb7b03
Richard Robertson <RichardRobertson836@yahoo.com>
2025-10-08 15:03:27 -0400

    Added weeks to front page

--- sportstracker_app/views.py
+++ sportstracker_app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render, HttpResponse
+from .models import Game
 
 # Create your views here.
 def home(request):
-    return render(request, 'main.html')
+    Weeks =["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18"]
+    Games = Game.objects.all()
+    context={
+        "Games":Games,
+        "Weeks":Weeks
+    }
+    return render(request, 'main.html',context)   
