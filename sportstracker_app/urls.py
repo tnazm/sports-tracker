4c1094c458c5112d29a9bef529f0a50bd0024663
Joshua Ejiofor <jue3@scarletmail.rutgers.edu>
2025-10-13 00:42:15 -0400

    Some changes
    Test of clickable button

--- sportstracker_app/urls.py
+++ sportstracker_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("", views.admin_dashboard, name="dashboard"),
+    path('games/', views.games_page,name='games'),
 ]
--- sportstracker_app/views.py
+++ sportstracker_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'main.html')
+
+def games_page(request):
+    return render(request, 'games.html')
