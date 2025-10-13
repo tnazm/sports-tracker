db4472e4cac527d437287305c11cec9c821b636d
Joshua Ejiofor <jue3@scarletmail.rutgers.edu>
2025-10-13 10:58:02 -0400

    Reapply "Merge branch 'main' of https://github.com/tnazm/sports-tracker"
    This reverts commit b44ead149c3044201b7144e75390505a6f54a1a9.

---
+++ Backend/BackendDB/GetGameIds.py
@@ -0,0 +1,49 @@
+import http.client
+import json
+from datetime import date,timedelta
+from dotenv import load_dotenv
+import os
+import django
+import sys
+load_dotenv()
+KEY=os.getenv("API_KEY")
+Today = date.today()
+## PATH TO BE ABLE TO ACCESS DJANGO MODELS
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sportstracker.settings')
+django.setup()
+#import Hold ID mode to make sure we are not adding duplicate IDS
+from sportstracker_app.models import HoldIDs
+## function to get all the game IDS for today and add them to the HoldIDs database (GameID, week, date)
+def GamesToday():
+    conn = http.client.HTTPSConnection("v1.american-football.api-sports.io")
+
+    headers = {
+        'x-rapidapi-host': "v1.american-football.api-sports.io",
+        'x-rapidapi-key': KEY
+        }
+
+    conn.request("GET", f"/games?league=1&date={Today}", headers=headers)
+
+    res = conn.getresponse()
+    data = res.read()
+
+    live_games=data.decode("utf-8")
+    parsed_live_games = json.loads(live_games) # make json readable
+
+    for game in parsed_live_games["response"]:
+        if not HoldIDs.objects.filter(GameID=game["game"]["id"]).exists():
+            HoldIDs.objects.create(GameID=game["game"]["id"],week=game["game"]["week"],date =game["game"]["date"]["date"])
+        ## ADDS GAME IDS TO DATABASE HAS TO BE RAN EVERYDAY
+
+
+
+GamesToday()
+
+
+
+Today =  Today + timedelta(days=1)    
+GamesToday()
+
+
