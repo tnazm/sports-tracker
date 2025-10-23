93ffab154e466bd235b92fb10de1455e318af506
Richard Robertson <RichardRobertson836@yahoo.com>
2025-10-23 12:13:50 -0400

    added more things to game summary box

--- Backend/BackendDB/Testing_script.py
+++ Backend/BackendDB/Testing_script.py
@@ -7,6 +7,7 @@ import django
 import sys
 load_dotenv()
 KEY=os.getenv("API_KEY")
+KEY2=os.getenv("2ndAPI_KEY")
 Today = date.today()
 import time
 ## PATH TO BE ABLE TO ACCESS DJANGO MODELS
@@ -15,14 +16,34 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sportstracker.settings')
 django.setup()
 #import Hold ID mode to make sure we are not adding duplicate IDS
-from sportstracker_app.models import HoldIDs, Game
+from sportstracker_app.models import HoldIDs, Game,GameData
 ## function to get all the game IDS for today and add them to the HoldIDs database (GameID, week, date)
 Games=Game.objects.values_list("GameID",flat=True)
-
+Gamedata=GameData.objects.values_list("GameID",flat=True)
 Ids=HoldIDs.objects.values_list('GameID',flat=True)
 
+def GameSummary(Gameid):
+    conn = http.client.HTTPSConnection("v1.american-football.api-sports.io")
+
+    headers = {
+        'x-rapidapi-host': "v1.american-football.api-sports.io",
+        'x-rapidapi-key': KEY2
+        }
+
+    conn.request("GET", f"/games/statistics/teams?id={Gameid}", headers=headers)
+
+    res = conn.getresponse()
+    data = res.read()
+
+    decoded =data.decode("utf-8")
+    parsed = json.loads(decoded)
+
+    return parsed
 
 for game in Games:
-    if game not in Ids:
-        print(game)
+    if game not in Gamedata:
+        GameData.objects.create(GameID=game, data=GameSummary(game))
+        time.sleep(6)
+        print("Added data for game:",game)
+    
 
